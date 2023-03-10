01ce27a48f01ae93080e7096a04e8fc2f2fe59d1
audiobird <cantrellhepner@gmail.com>
2023-03-10 13:26:58 -0800

    clean up.

---
+++ include/main.h
@@ -0,0 +1,7 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
-#include <avr/io.h>
-#include <util/delay.h>
+#include "main.h"
 
 int main()
 {
